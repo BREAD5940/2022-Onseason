ebd124e972792a4d58a463f4aeefb3b11b385828
KrishKittur <73321517+KrishKittur@users.noreply.github.com>
2022-02-05 22:53:19 -0800

    Initial Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,57 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  @Override
+  public void robotInit() {
+    m_robotContainer = new RobotContainer();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+
+  public RobotContainer() {
+    configureButtonBindings();
+  }
+
+  private void configureButtonBindings() {}
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
